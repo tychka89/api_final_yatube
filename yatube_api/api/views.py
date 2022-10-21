bca63a32e81cd490b40e66196bc3d751e47b07bc
Nataliya Rodikova <bachininang@gmail.com>
2022-10-21 09:27:40 +0500

    sprint9project3

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import get_object_or_404
 
 from rest_framework import filters, mixins, viewsets
-# from rest_framework import permissions
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.pagination import LimitOffsetPagination
 
